9b92b3f1dbfec509c965566b2f22c3e042deac29
hugocampuseni2022 <hugo.tessier2022@campus-eni.fr>
2022-08-22 16:54:44 +0200

    Class sortie

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -50,8 +50,9 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new Response("test");
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
