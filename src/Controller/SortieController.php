2e100796f3cd1da20bf38c69b106dd6110ee69eb
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-08-23 11:34:30 +0200

    page annuler une sortie v1

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -79,12 +79,26 @@ class SortieController extends AbstractController
     }
 
     /**
+     * @Route("/{id}/delete", name="app_sortie_delete_page", methods={"GET"})
+     */
+    public function deletePage(Sortie $sortie): Response
+    {
+        return $this->render('sortie/delete.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+
+    /**
      * @Route("/{id}", name="app_sortie_delete", methods={"POST"})
      */
     public function delete(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$sortie->getId(), $request->request->get('_token'))) {
             $sortieRepository->remove($sortie, true);
+
+            $this->addFlash('success','Cette sortie a bien été supprimer!');
+
         }
 
         return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
