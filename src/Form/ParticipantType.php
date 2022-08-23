b3452375b6c9f48acbac12ffdd41acd58c0b2723
hugocampuseni2022 <hugo.tessier2022@campus-eni.fr>
2022-08-23 14:42:34 +0200

    Class sortie

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -5,31 +5,52 @@ namespace App\Form;
 use App\Entity\Campus;
 use App\Entity\Sortie;
 use App\Entity\Participant;
+use phpDocumentor\Reflection\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ParticipantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('nom')
+            ->add('pseudo')
             ->add('prenom')
+            ->add('nom')
             ->add('telephone')
-            ->add('administrateur')
-            ->add('actif')
+            ->add('email')
+
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
             ->add('campus',EntityType::class,[
                 'class'=>Campus::class,
                 'choice_label'=>'nom',
-            ])
-            ->add('sorties',EntityType::class,[
-                'class'=>Sortie::class,
-                'choice_label'=>'nom',
-                'multiple'=>true,
             ])
             ->add('photo')
         ;
