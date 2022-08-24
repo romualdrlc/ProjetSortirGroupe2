c29dca7ee8f9d8c2377019b9b0c3c5751adcc405
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-08-24 16:39:02 +0200

    role admin

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Campus;
 use App\Form\CampusType;
 use App\Repository\CampusRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/campus")
+ * @IsGranted("ROLE_ADMIN")
  */
 class CampusController extends AbstractController
 {
--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Ville;
 use App\Form\VilleType;
 use App\Repository\VilleRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/ville")
+ * @IsGranted("ROLE_ADMIN")
  */
 class VilleController extends AbstractController
 {
