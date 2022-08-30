ced81660fa3cae87c64fa1ff4019de81714cf389
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-08-30 15:52:18 +0200

    intégration csv

---
+++ src/Command/UserIntegrationCommand.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Participant;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserIntegrationCommand extends Command
+{
+
+    protected static $defaultName = 'app:UserIntegration';
+    protected static $defaultDescription = 'Intégration de participant';
+    private string $csv; // La route du fichier csv est importée grâce au fichier services.yaml
+    private UserPasswordHasherInterface $hashedPassword;
+    private EntityManagerInterface $manager;
+
+    public function finder()
+    {
+        $finder = new Finder();
+        $absoluteFilePath = null;
+
+        $finder->files()->in('public/asset/csv');
+
+        if ($finder->hasResults()){
+            foreach ($finder as $file) {
+                $absoluteFilePath = $file->getRealPath();
+            }
+        }
+
+        return $absoluteFilePath;
+
+    }
+
+    /**
+     * ImportCommand constructor.
+     * @param string $csv
+     * @param UserPasswordHasherInterface $hashedPassword
+     * @param EntityManagerInterface $manager
+     */
+    public function __construct(UserPasswordHasherInterface $hashedPassword, EntityManagerInterface $manager)
+    {
+        parent::__construct(self::$defaultName);
+        $this->csv = $this->finder();
+        $this->hashedPassword = $hashedPassword;
+        $this->manager = $manager;
+    }
+
+    protected function configure()
+    {
+        $this->setHelp('Permet l\'intégration de participants par un fichier csv');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if (($handle = fopen($this->csv, "r")) !== false) {
+            while (($line = fgetcsv($handle, 1000, ";")) !== false) {
+                $participant = new Participant();
+                $participant
+                    ->setNom($line[0])
+                    ->setPrenom($line[1])
+                    ->setEmail($line[2])
+                    ->setRoles((array)'ROLE_USER')
+                    ->setPseudo($line[3])
+                    ->setUpdatedAt(new \DateTime());
+
+                $participant->setPassword($this->hashedPassword->hashPassword($participant, "password"));
+
+                $this->manager->persist($participant);
+
+            }
+
+            unlink($this->csv);
+
+            fclose($handle);
+        }
+        $this->manager->flush();
+        $io->success('Votre commande est un franc succès');
+        return Command::SUCCESS;
+
+    }
+
+}
---
+++ src/Form/ParticipantCsvType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantCsvType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('upload_file', FileType::class, [
+                'label' => 'fichier d\'inscription (.csv)',
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [ // We want to let upload only txt, csv or Excel files
+                            'text/x-comma-separated-values',
+                            'text/comma-separated-values',
+                            'text/x-csv',
+                            'text/csv',
+                            'text/plain',
+                            'application/octet-stream',
+                            'application/vnd.ms-excel',
+                            'application/x-csv',
+                            'application/csv',
+                            'application/excel',
+                            'application/vnd.msexcel',
+                            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                        ],
+                        'mimeTypesMessage' => "This document isn't valid.",
+                    ])
+                ],
+            ])
+            ->add('send', SubmitType::class);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+/*        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);*/
+    }
+}
