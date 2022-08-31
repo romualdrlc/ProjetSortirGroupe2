220607c80c9f65242b8fdabe1b11d853c19f2609
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-08-31 09:42:52 +0200

    datetime dans le formulaire

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -8,7 +8,6 @@ use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
