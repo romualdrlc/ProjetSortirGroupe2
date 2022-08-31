4965f5d5e8cd1074ab23f08028863bbc34171799
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-08-31 11:30:48 +0200

    methodes pour activer/désactiver un participant

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -119,4 +119,48 @@ class ParticipantController extends AbstractController
 
         return $this->redirectToRoute('app_participant_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    /**
+     * @Route("/activer/{participant}", name="app_participant_activer")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function activerUtilisateur(
+        Participant $participant,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $inactif = $participant;
+
+        if (!$inactif->isActif()){
+            $inactif->setActif(true);
+            $entityManager->persist($inactif);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participant_index');
+        }
+
+        return $this->render('participant/show.html.twig');
+    }
+
+    /**
+     * @Route("/desactiver/{participant}", name="app_participant_desactiver")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function desactiverUtilisateur(
+        Participant $participant,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $inactif = $participant;
+
+        if ($inactif->isActif()){
+            $inactif->setActif(false);
+            $entityManager->persist($inactif);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participant_index');
+        }
+
+        return $this->render('participant/show.html.twig');
+    }
 }
